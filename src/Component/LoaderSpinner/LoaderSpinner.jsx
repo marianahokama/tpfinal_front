8acc084d206cc074b999ea56a5c8fb84c63e7efa
laujuvi <laujuvi@hotmail.com>
2025-07-03 21:27:27 -0300

    ultimo eliminar

--- src/Component/LoaderSpinner/LoaderSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import './LoaderSpinner.css'
-export default function LoaderSpinner() {
-    return (
-        <div className="spinner-container">
-            <div className="spinner"></div>
-        </div>
-    )
-}
--- src/services/contactService.js
+++
@@ -1,6 +0,0 @@
-import mook_data from "../data/contact-mook"
-
-
-export const getContactList = () => {
-    return mook_data.contacts
-}
